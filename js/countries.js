6f19b80ed2311efc0de559b57ced415ff1b3c351
Mushfiq80 <aurpon80@gmail.com>
2023-03-21 08:02:26 +0600

    destructering

--- js/countries.js
+++ js/countries.js
@@ -7,18 +7,31 @@ const loadCountries = () => {
 const displayCountries = countries => {
     // console.log(countries);
     const allCountriesHTML = countries.map(country => getCountryHTML(country));
-    console.log(allCountriesHTML);
+    // console.log(allCountriesHTML);
     const container = document.getElementById('countries');
     container.innerHTML = allCountriesHTML.join('');
 }
 
-const getCountryHTML = country => {
+// const getCountryHTML = country => {
+//     // option 1 
+//     return `
+//         <div class="country">
+//             <h2>${country.name.common}</h2>
+//             <img src="${country.flags.png}" alt="${country.name.common}">
+//             <p>${country.capital}</p>
+//             <button onclick="loadCountryByName('${country.name.common}')">Details</button>
+//         </div>
+//     `;
+// }
+const getCountryHTML = ({name, flags, capital}) => {
+    // option 2
+    // const {name, flags, capital} = country; //parameter destructuring
     return `
         <div class="country">
-            <h2>${country.name.common}</h2>
-            <img src="${country.flags.png}" alt="${country.name.common}">
-            <p>${country.capital}</p>
-            <button onclick="loadCountryByName('${country.name.common}')">Details</button>
+            <h2>${name.common}</h2>
+            <img src="${flags.png}" alt="${name.common}">
+            <p> ${capital}</p>
+            <button onclick="loadCountryByName('${name.common}')">Details</button>
         </div>
     `;
 }
